f016f435153426752137dea4c709cce7bc078dee
Fitgerald Afful <fitzafful@gmail.com>
2020-04-04 17:07:59 +0000

    Added AppIcons and Launch Image to Assets'

---
+++ CoronaVirus/Controllers/HomeController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeController.swift
+//  CoronaVirus
+//
+//  Created by Fitzgerald Afful on 04/04/2020.
+//  Copyright © 2020 Fitzgerald Afful. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
