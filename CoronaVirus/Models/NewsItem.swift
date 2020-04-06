5b1df434d043f9e24ba458a857e3f7687655edb5
Fitgerald Afful <fitzafful@gmail.com>
2020-04-06 01:52:11 +0000

    Made updates to News Item model to capture Image url from rss

--- CoronaVirus/Models/NewsItem.swift
+++ CoronaVirus/Models/NewsItem.swift
@@ -13,25 +13,23 @@ public class NewsItem : Codable, Equatable {
     var description: String
     var link: String
     var date: Date
+    var imageUrl: URL? = nil
 
     init(title:String, description: String, link: String, date: Date) {
         self.title = title
         self.description = description
         self.link = link
         self.date = date
+        if description.components(separatedBy: "\" alt=\"").count > 1 {
+            let firstImageString = description.components(separatedBy: "\" alt=\"")[0]
+            let imageString = firstImageString.components(separatedBy: "src=\"")[1]
+            print(imageString)
+            self.imageUrl = URL(string: imageString)
+        }
     }
 
     public static func == (lhs: NewsItem, rhs: NewsItem) -> Bool {
         return (lhs.link == rhs.link)
-    }
-
-    var imageUrl: URL? {
-        if description.components(separatedBy: "\" alt=\"").count > 1 {
-            let firstImageString = description.components(separatedBy: "\" alt=\"")[0]
-            let imageString = firstImageString.components(separatedBy: "src=\"")[1]
-            return URL(string: imageString)
-        }
-        return nil
     }
 }
 
